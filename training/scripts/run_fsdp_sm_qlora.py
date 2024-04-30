5b9e1c73eb510b37fa1609c2217d0bb37735f718
chengjian <ytcheng@qq.com>
2024-04-30 10:47:25 +0800

    Update run_fsdp_sm_qlora.py

--- training/scripts/run_fsdp_sm_qlora.py
+++ training/scripts/run_fsdp_sm_qlora.py
@@ -41,7 +41,7 @@ LLAMA_3_CHAT_TEMPLATE = (
 )
 """
 LLAMA_3_CHAT_TEMPLATE = (
-    "{{'<|begin_of_text|>' + messages}}"
+    "{{'<|begin_of_text|>' + messages + '<|end_of_text|>'}}"
 )
 
 # ACCELERATE_USE_FSDP=1 FSDP_CPU_RAM_EFFICIENT_LOADING=1 torchrun --nproc_per_node=4 ./scripts/run_fsdp_qlora.py --config llama_3_70b_fsdp_qlora.yaml
