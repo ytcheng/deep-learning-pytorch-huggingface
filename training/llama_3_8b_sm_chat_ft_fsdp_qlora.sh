0811079a9dd2811128977224176829829de0e2b0
程建(橙子) <chengjian@founq.com>
2024-05-08 12:46:25 +0000

    fintuning

--- training/llama_3_8b_sm_chat_ft_fsdp_qlora.sh
+++ training/llama_3_8b_sm_chat_ft_fsdp_qlora.sh
@@ -1,1 +1,1 @@
-ACCELERATE_USE_FSDP=1 FSDP_CPU_RAM_EFFICIENT_LOADING=1 torchrun --nproc_per_node=4 ./scripts/run_fsdp_sm_ft_chat_qlora.py --config llama_3_8b_sm_chat_ft_fsdp_qlora.yaml
+ACCELERATE_USE_FSDP=1 FSDP_CPU_RAM_EFFICIENT_LOADING=1 torchrun --nproc_per_node=1 ./scripts/run_fsdp_sm_ft_chat_qlora.py --config llama_3_8b_sm_chat_ft_fsdp_qlora.yaml
